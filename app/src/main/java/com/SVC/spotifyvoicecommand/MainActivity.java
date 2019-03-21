6bf9af138741bdac207ce78fde97807b00b5baaf
ann-tbal <41850019+ann-tbal@users.noreply.github.com>
2019-03-21 14:47:35 -0400

    changed redirect uri

--- app/src/main/java/com/SVC/spotifyvoicecommand/MainActivity.java
+++ app/src/main/java/com/SVC/spotifyvoicecommand/MainActivity.java
@@ -3,17 +3,30 @@ package com.SVC.spotifyvoicecommand;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1138;
-    private static final String REDIRECT_URI = "com.SVC.spotifyvoicecommand";
+    private static final String REDIRECT_URI = "com.SVC.spotifyvoicecommand://callback";
     private static final String CLIENT_ID = "92348339626f44faa05efdedf8ac93d1";
     private static final String [] scopes = {"app-remote-control", "streaming"};
+
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     protected void openLoginActivity () {
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
@@ -27,25 +40,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openLoginActivity();
+//        openLoginActivity();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
 
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
 
-    }
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
 
-    private void connected() {
-        // Then we will write some more code here.
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // Aaand we will finish off here.
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:0fZm7ygIaFLpTX7AEd38WT");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
@@ -58,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
                 // Response was successful and contains auth token
                 case TOKEN:
                     // Handle successful response
+                    connected();
                     break;
 
                 // Auth flow returned an error
