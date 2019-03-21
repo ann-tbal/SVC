f125f5c6e79dc6e4dbd1df057de899cf4f65d00d
ann-tbal <41850019+ann-tbal@users.noreply.github.com>
2019-03-21 12:46:15 -0400

    handling authentication

--- app/src/main/java/com/SVC/spotifyvoicecommand/MainActivity.java
+++ app/src/main/java/com/SVC/spotifyvoicecommand/MainActivity.java
@@ -1,13 +1,74 @@
 package com.SVC.spotifyvoicecommand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1138;
+    private static final String REDIRECT_URI = "com.SVC.spotifyvoicecommand";
+    private static final String CLIENT_ID = "92348339626f44faa05efdedf8ac93d1";
+    private static final String [] scopes = {"app-remote-control", "streaming"};
+
+    protected void openLoginActivity () {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(scopes);
+
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openLoginActivity();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
     }
 }
