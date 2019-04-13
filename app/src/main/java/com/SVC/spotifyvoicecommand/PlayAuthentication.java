6aec223140e5fd2e21f12e11bda629681de7d647
ann-tbal <41850019+ann-tbal@users.noreply.github.com>
2019-04-13 14:46:11 -0400

    added retrofit

--- app/src/main/java/com/SVC/spotifyvoicecommand/PlayAuthentication.java
+++ app/src/main/java/com/SVC/spotifyvoicecommand/PlayAuthentication.java
@@ -1,5 +1,6 @@
 package com.SVC.spotifyvoicecommand;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
@@ -12,19 +13,18 @@ import java.security.SecureRandom;
 
 public class PlayAuthentication extends AppCompatActivity {
 
-    private static String accessToken = "";
     private String CLIENT_ID = "";
     private String REDIRECT_URI = "";
+    private String [] scopes = {};
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // required info for authentication
-        REDIRECT_URI = getString(R.string.redirect_uri);
         CLIENT_ID = getString(R.string.client_id);
-
+        REDIRECT_URI = getString(R.string.redirect_uri);
+        scopes = getResources().getStringArray(R.array.scopes);
 
         createAuthURL();
 
@@ -36,6 +36,7 @@ public class PlayAuthentication extends AppCompatActivity {
         byte[] code = new byte[32];
         secureRandom.nextBytes(code);
         verifier = android.util.Base64.encodeToString(code, Base64.NO_WRAP | Base64.NO_PADDING | Base64.NO_WRAP);
+        Log.e("creating verifier: ", verifier);
         return verifier;
     }
 
@@ -43,11 +44,12 @@ public class PlayAuthentication extends AppCompatActivity {
         String challenge = "";
         try {
             byte[] bytes = verifier.getBytes("US-ASCII");
+
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(bytes, 0, bytes.length);
             byte[] digest = messageDigest.digest();
-            challenge = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(digest);
-
+            challenge = Base64.encodeToString(digest, Base64.URL_SAFE, Base64.NO_PADDING, Base64.NO_WRAP);
+            Log.e("creating challenge: ", challenge);
         } catch (UnsupportedEncodingException e) {
             Log.e("Creating challenge: ", e.toString()) ;
         } catch (NoSuchAlgorithmException e) {
@@ -58,14 +60,21 @@ public class PlayAuthentication extends AppCompatActivity {
     }
 
     private String createAuthURL () {
+        // formats the scopes
+        String scopesString = "";
+        for (String item: scopes) {
+            scopesString = scopesString + item + "%20";
+        }
+
         String authURL = "https://accounts.spotify.com/authorize" +
                 "?response_type=code" +
                 "?client_id=" + CLIENT_ID +
-                "?code_ challenge=" + createChallenge(createVerifier()) +
+                "?code_challenge=" + createChallenge(createVerifier()) +
                 "?code_challenge_method=S256" +
-                "?redirect_uri=" + REDIRECT_URI;
+                "?redirect_uri=" + REDIRECT_URI +
+                "?scope=" + scopesString; 
 
-        Log.d("CreateAuthUrl", authURL);
+        Log.e("creating auth URL: ", authURL);
 
         return authURL;
     }
